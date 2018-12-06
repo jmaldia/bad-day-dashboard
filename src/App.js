3b431941432518666287892a4fe9261929a9c8b8
Jon Maldia <jon@maldia.net>
2018-12-06 13:08:15 -0500

    Add barebones components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Calendar from './components/Calendar';
+import Email from './components/Email';
+import News from './components/News';
+import Todo from './components/Todo';
+import Weather from './components/Weather';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Calendar />
+        <Email />
+        <News />
+        <Todo />
+        <Weather />
       </div>
     );
   }
