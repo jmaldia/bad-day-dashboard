b9e4d5756fb87712a94c36a262fd4b6205dacf2c
Jon Maldia <jon@maldia.net>
2018-12-06 13:34:24 -0500

    Add boilerplate for News component

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
 const News = (props) => {
+    const news = [
+        {headline: 'Trump vs Mueller', preview: 'This is the preview for your first news item'},
+        {headline: 'RIP George H W Bush', preview: 'This is the preview for your second news item'},
+        {headline: 'It\'s Sunny in California', preview: 'This is the preview for your third news item'},
+        {headline: 'Lakers sweep the Bulls', preview: 'This is the preview for your fourth news item'}
+    ]
     return (
-        <div style={styles}>
         
-        </div>
+                <div style={styles} className="News">
+                    <h2>News for you</h2>
+                    {
+                        news.map((item) => {
+                            return (
+                                <div>
+                                    <img src='#' alt='Contact'></img>
+                                    <h3>{item.headline}</h3>
+                                    <p>{item.preview}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
     );
 };
 
