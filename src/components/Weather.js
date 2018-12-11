d901186fcbc3081a7709d0a411ef5a5c5d84a5b4
Jon Maldia <jon@maldia.net>
2018-12-10 23:51:02 -0500

    Add boilerplate for weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,10 @@ const Weather
  = (props) => {
     return (
         <div style={styles}>
-        
+            <h2>Weather</h2>
+            <h3>35 degrees</h3>
+            <div className="Weather-forecast">
+            </div>
         </div>
     );
 };
