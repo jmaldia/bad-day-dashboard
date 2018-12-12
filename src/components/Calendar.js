424f233fbbf7505b2cf0dadf52a12c9fbe6e4f16
Jon Maldia <jon@maldia.net>
2018-12-11 22:20:54 -0500

    Add boilerplate for Calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,13 +3,27 @@ import React from 'react'
 const Calendar = (props) => {
     return (
         <div style={styles}>
-        
+            <h3>Agenda</h3>
+            <div>
+                <div>
+                    <h4>Agenda 1</h4>
+                    <p>Details</p>
+                </div>
+                <div>
+                    <h4>Agenda 1</h4>
+                    <p>Details</p>
+                </div>
+                <div>
+                    <h4>Agenda 1</h4>
+                    <p>Details</p>
+                </div>
+            </div>
         </div>
     );
 };
 
 const styles = {
-    
+
 };
 
 export default Calendar;
