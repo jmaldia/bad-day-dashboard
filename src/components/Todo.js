349a98fb21507f3814a5472dbbbb96c8ede6dea6
Jon Maldia <jon@maldia.net>
2018-12-29 06:40:07 -0500

    Fix issues with Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,27 +8,38 @@ const Todo = (props) => {
     //     {title: 'Pack'},
     //     {title: 'Lunch'}
     // ];
-   
-    return (
-        <div style={styles.container}>
-            <h3>Your Todos</h3>
-            {
-                this.props.todos ? this.props.todos.map((todo, index) => {
-                    if (index < 6) {
-                        return (
-                            <div key={index}>
-                                <img src='#' alt='Contact'></img>
-                                <h4>{todo && todo.project_id}</h4>
-                                <p><a href={todo && todo.url}>My Todo</a></p>
-                            </div>
-                        )
-                    } else {
-                        return undefined;
-                    }
-                }) : undefined
-            }
-        </div>
-    );
+    console.log(props.todo)
+    
+    if (!props.todo){
+        return (
+            <div style={styles.container}>
+                <h3>Your Todos</h3>
+                
+            </div>
+        );
+    } else {
+        return (
+            <div style={styles.container}>
+                <h3>Your Todos</h3>
+                {
+                    props.todos.map((todo, index) => {
+                        if (index < 6) {
+                            return (
+                                <div key={index}>
+                                    <img src='#' alt='Contact'></img>
+                                    <h4>{todo.project_id}</h4>
+                                    <p><a href={todo.url}>My Todo</a></p>
+                                </div>
+                            )
+                        } else {
+                            return undefined;
+                        }
+                    })
+                }
+            </div>
+        )
+    }
+
 };
 
 const styles = {
